a531b1b8fdc3a88a188ed437b7cbb0f46ef7da95
Vincent Agnano <vincent.agnano@scopyleft.fr>
2013-04-24 02:13:28 +0200

    added smooth scrolling

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 $(function() {
+    // fixed header position
     $('body > header').scrollToFixed();
+
+    // scroll smooth
+    $('[role=navigation] a').click(function() {
+        var href = $.attr(this, 'href');
+        $('html,body').animate({
+            scrollTop: $(href).offset().top
+        }, 500, function () {
+            window.location.hash = href;
+    });
+    return false;
+});
 });
