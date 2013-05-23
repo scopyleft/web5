5bce8d35f2df8ded700be776be1d0f91c3b9d95e
Vincent Agnano <vincent.agnano@scopyleft.fr>
2013-05-24 00:07:28 +0200

    fixed schedules.json and added body and title to speakers

--- scrappers/speakers.js
+++ scrappers/speakers.js
@@ -6,12 +6,14 @@ casper.start('http://conf2013.web-5.org/en/web-5-speakers-en/', function() {
         var articles = document.querySelectorAll('#content[role="main"] .hofMember');
         return Array.prototype.map.call(articles, function(e) {
             return {
-                img: e.querySelectorAll('.hofPicLink img')[0].getAttribute('src'),
-                name: e.querySelectorAll('.speakerAnchor')[0].innerHTML,
-                website: e.querySelectorAll('.hofPicLink > div > a:first-child')[0].getAttribute('href'),
-                twitter: e.querySelectorAll('.hofPicLink > div > a:last-child')[0].getAttribute('href'),
+                img: e.querySelector('.hofPicLink img').getAttribute('src'),
+                name: e.querySelector('.speakerAnchor').innerHTML,
+                website: e.querySelector('.hofPicLink > div > a:first-child').getAttribute('href'),
+                twitter: e.querySelector('.hofPicLink > div > a:last-child').getAttribute('href'),
                 bio: [].map.call(e.querySelectorAll('.hofBio p'), function(p) {return p.innerHTML;}),
-                summary: e.querySelectorAll('.hofBio p')[0].innerHTML.substr(0, 50)
+                summary: e.querySelector('.hofBio p').innerHTML.substr(0, 50),
+                title: e.nextElementSibling.querySelector('h1').innerText.substr(15),
+                body: [].map.call(e.nextElementSibling.querySelectorAll('p'), function(p) {return p.innerText}).join("\n"),
             };
         });
     });
